6906c6bbb6932fe3a7e220675aacd0f062c2238e
Chrystal Copeland <canderson73@cnm.edu>
2019-06-26 22:11:20 -0600

    found error in code, but I am still unable to add items to the list.

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ window.onload = function () {
 	initShoppingList ();
 };
 
-function initShoppingList () {
+function initShoppingList() {
 	let form = document.getElementById("item-form");
 
 	form.addEventListener("submit", (event) => {
-		hanleItemForm(event, form);
+		handleItemForm(event, form);
 	});
 }
 
@@ -16,44 +16,45 @@ function handleItemForm(event, formRef) {
 	}
 
 	addItemToShoppingList();
-	formRef.reset();
+
 	return false;
-}
 
-function addItemToShoppingList() {
-	let itemName = document.getElementById("item-name");
-	let itemAmount = document.getElementById("item-amount");
-	let id = getRandomInt(0, 10000000)
+	function addItemToShoppingList() {
+		let itemName = document.getElementById("item-name");
+		let itemAmount = document.getElementById("item-amount");
+		let id = getRandomInt(0, 10000000)
 
-	// creates list item html and appends to page
-	let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
-		console.log ("Item HTML: ", itemHtml);
-	let itemListRef = document.getElementById("shopping-list");
-	itemListRef.insertAdjacentHTML("afterend", itemHtml);
+		// creates list item html and appends to page
+		let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+		console.log("Item HTML: ", itemHtml);
+		let itemListRef = document.getElementById("shopping-list");
+		itemListRef.insertAdjacentHTML("afterend", itemHtml);
 
-	setDeleteButtonEvent(id);
-}
+		setDeleteButtonEvent(id);
+	}
 
-function setDeleteButtonEvent (id) {
-	let deleteButton = document.getElementById("button"+id);
-	deleteButton.addEventListener("click", () => {
-		console.log("DeleteButton Works");
-	});
-}
+	function setDeleteButtonEvent(id) {
+		let deleteButton = document.getElementById("button" + id);
+		deleteButton.addEventListener("click", () => {
+			console.log("DeleteButton Works");
+		});
+	}
 
-function createListItemHtml(itemName, itemAmount, id) {
-	return `<li id="item${id}">
+	function createListItemHtml(itemName, itemAmount, id) {
+		return `<li id="item${id}">
 					${itemName} - ${itemAmount} 
 					<button id="button${id}" type="button">Delete Item</button>  
 				</li>`;
-}
+	}
 
-function removeListItem() {
-	let listItem = document.getElementByID("item"+id)
-	listItem.parentNode.removeChild(listItem);
-}
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max-min)) + min;
+	function removeListItem() {
+		let listItem = document.getElementByID("item" + id)
+		listItem.parentNode.removeChild(listItem);
+	}
+
+	function getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
 }
